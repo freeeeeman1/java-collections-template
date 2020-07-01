0ed24e1dc2a48915cead6362a49627103f02a8d3
Smolyakov Vadim <gamesboysmyname@icloud.com>
2020-07-01 23:54:48 +0300

    well done!

--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,10 +2,12 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Collections.*;
 
@@ -16,36 +18,48 @@ import static java.util.Collections.*;
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        return this.getWords(text).stream().mapToInt(String::length).sum();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        return this.getWords(text).stream().mapToInt(n -> 1).sum();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        return this.getUniqueWords(text).stream().mapToInt(n -> 1).sum();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+        List<String> stringList = new ArrayList<>();
+        Pattern p = Pattern.compile("[a-zA-Z]+");
+        Matcher m = p.matcher(text);
+        while (m.find()) {
+            stringList.add(m.group());
+        }
+        return stringList;
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+        return this.getWords(text).stream().distinct().collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return emptyMap();
+        return this.getWords(text).stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.reducing(0, n -> 1, Integer::sum)));
+//        return emptyMap();
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        if (direction.equals(Direction.ASC)) {
+            return this.getWords(text).stream().sorted((str1, str2) -> str1.length() - str2.length()).collect(Collectors.toList());
+        } else {
+            return this.getWords(text).stream().sorted((str1, str2) -> str2.length() - str1.length()).collect(Collectors.toList());
+        }
     }
 }
